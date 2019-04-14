728a649352c152aedfffedc3eb53b5eaa88de753
Björn Friedrichs <bjoern@friedrichs1.de>
2019-04-14 16:05:45 +0100

    Add dev / prod switch

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,4 +1,6 @@
-export const API_URL = "http://192.168.0.25:8000/";
+import { API } from "react-native-dotenv";
+
+export const API_URL = "" + API;
 export const BASE_URL = "https://resetera.com";
 
 export const ENDPOINTS = {
